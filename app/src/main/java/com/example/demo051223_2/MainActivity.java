42d74e123049cfff560a2e2ee05e7ebb06e6db56
tv0823 <tv0823@bs.amalnet.k12.il>
2023-12-05 09:14:08 +0200

    last commit

--- app/src/main/java/com/example/demo051223_2/MainActivity.java
+++ app/src/main/java/com/example/demo051223_2/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.demo051223_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    int clicked = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void numberClicks(View view) {
+        Button btn;
+        btn = findViewById(R.id.btn);
+        clicked ++;
+        btn.setText("This is a click number: " + clicked);
     }
 }
